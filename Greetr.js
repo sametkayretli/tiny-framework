10eec8c2daa07cf76e8f6d529e98b4012c1b0c64
sametkayretli <sametkayretli@gmail.com>
2020-05-11 17:24:36 +0300

    updated and added jQuery support

--- Greetr.js
+++ Greetr.js
@@ -1,32 +1,38 @@
 (function(global, $){
 
+   // Creating variable Greetr and put a "new" object to it
    var Greetr = function(firstName, lastName, language){
       return new Greetr.init(firstName, lastName, language);
    }
 
+   // Hidden within the scope of the IIFE  and never directly accessible
    var supportedLangs = ["en", "tr"];
 
+   // Informal greetings
    var greetings = {
       en: "Hello",
       tr: "Selam"
    };
 
+   // Formal greetings
    var formalGreetings = {
       en: "Greetings",
       tr: "Merhaba"
    };
 
+   // Logger messages
    var logMessages = {
       en: "Logged In",
       tr: "Giriş Yapıldı"
    };
 
 
-
+   // The prototype that holds methods (to save memory space)
    Greetr.prototype = {
 
       // adding methods (functionallity)
 
+      // "this" refers to the calling object at execution time
       fullName: function(){
       
          return this.firstName + " " + this.lastName;
@@ -35,7 +41,9 @@
 
 
       validate: function(){
-   
+
+         // Check that is a valid language
+         // References the externally inaccessible "supportedLangs" within the closure 
          if(supportedLangs.indexOf(this.language) === -1){
    
             throw "Invalid Language";
@@ -43,7 +51,7 @@
          }
       },
 
-
+      // Rerieve messages from object by referring to properties using [] syntax
       greeting: function(){
    
          return greetings[this.language] + " " + this.firstName + "!";
@@ -57,6 +65,7 @@
    
       },
 
+      // Chainable methods return their own containing object
       greet: function(formal){
 
          var msg;
@@ -93,21 +102,60 @@
 
             console.log(logMessages[this.language] + ": " + this.fullName());
          }
+
+         // Make chainable
          return this;
       },
 
       setLang: function(lang){
          
+         // Set the language
          this.language = lang;
+
+         //validate
          this.validate();
 
+         // Make chainable
+         return this;
+      },
+
+      HTMLGreeting: function(selector, formal){
+         if(!$){
+
+            throw "jQuery not loaded";
+         } 
+         
+         if(!selector){
+         
+            throw "Missing jQuery selector";
+         }
+
+         // Determine the message
+         var msg;
+
+         if(formal){
+
+            msg = this.formalGreeting();
+         
+         } else {
+            
+            msg = this.greeting();
+         }
+
+         // Inject the message in the chosen place in the DOM
+         $(selector).html(msg);
+
+         // Make chainable
          return this;
       }
 
    };
    
    
-   // this is the actual object
+   /* 
+   This is the actual object created here, allowing us to "new" an object
+   without calling "new"
+   */
    Greetr.init = function(firstName, lastName, language){
       
       //setting up this to self for safe
@@ -119,8 +167,11 @@
       self.lastName = lastName || "";
       self.language = language || "en";
 
+      self.validate();
+
    }
 
+   // Trick borrowed from jQuery so we don't have to use the "new" keyword
    Greetr.init.prototype = Greetr.prototype;
 
    /*
--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+/*
+Gets a new object (the architecture allows us to not have to
+use the "new" keyword here)
+*/
 var s = G$("samet", "kayretli");
 
-s.greet();
-s.greet(true);
-s.greet().setLang("tr").greet(true);
+// use our chainable methods
+s.greet().setLang("tr").greet(true).log();
+
+// When you click the login button jQuery "click" method the function is invoked
+$("#login").click(function(){
+
+   // Create a variable for Greetr.js
+   var loginGrtr = G$("samet", "kayretli");
+
+   // Hide the login division
+   $("#logindiv").hide();
+
+   /*
+   1. get the data for Greetr.js
+   2. set the language by retrieving from #lang selector value
+   3. invoke HTMLGreeting function
+   4. Print the log message by invoking log function
+   */
+   loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
+});
